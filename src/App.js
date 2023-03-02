80c86c68ec6f4687e69db543833d080f82d0340b
Manju Madalabhanvi <manju.madalabhanvi@publicissapient.com>
2023-03-02 15:22:43 +0530

    for checking the render issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import OnlineNav from './Megha/navbar/nav';
 import Navbar from './Manju/navbar';
 
+// const OnlineNav =React.lazy(()=>import('../src/Megha/navbar/nav.js') )
+// const Navbar=React.lazy(()=> import('../src/Manju/navbar.js'))
+
+
 function App() {
+  // const path=[
+  //   {path:'/meghanav', element:<OnlineNav/>},
+  //   {path:'/manju', element: <Navbar/> }
+
+  //   ]
+    
   return (
     <div className="App">
-      
-     <OnlineNav/>
-     <Navbar/>      
+    <Navbar/>
+   
+
+         
     </div>
   );
 }
--- src/Manju/navbar.js
+++ src/Manju/navbar.js
@@ -1,26 +1,42 @@
 import React from 'react';
+import './navbar.css';
+import callicon from '../../src/Assets/callicon.svg';
+import navicon from "../../src/Assets/navlogo.svg"
 
-const Navbar=()=>
-{
-    return(
-        <div className="navbar">
-    <nav>
-        <li>
-            <select>
-              
-                <option>Furniture </option>
-                <option>Dresses </option>
-                <option>Shoes</option>
-                <option>Books</option>
-                <option>Mobiles</option>
-            </select>
-         </li>
-         </nav>
-    </div>
-
+const Navbar = () => {
+  return (
+    <>
+    <div className="navbar-top">
+       <img id="callogo" src={callicon}/>
+       <p>+919822220005</p>
+       <p id="offerline">Get 50% Off on Selected Items    |      Shop Now</p>
+        </div>
+        
+    <div className="navbar-main">
       
-
+        <div className="nav-logo">
+        <img id="nav-icon" src={navicon}/>
+        </div>
+    <div className="navbar-items">
+        <li>
+          <select>
+            <option>Category </option>
+            <option>Dresses </option>
+            <option>Shoes</option>
+            <option>Books</option>
+            <option>Mobiles</option>
+          </select>
+        </li>
+        <li>Deals</li>
+        <li>What's New</li>
+        <li>Delivery</li>
+        </div>
+      
+      </div>
+      </>
     
-    );
+    
+   
+  )
 }
-export default Navbar;
+export default Navbar
