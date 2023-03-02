2ff992576f9f9e3a6dbe16f453f802a9b760542b
Manju Madalabhanvi <manju.madalabhanvi@publicissapient.com>
2023-03-02 13:10:02 +0530

    imagefolder

--- src/Manju/navbar.js
+++ src/Manju/navbar.js
@@ -1,10 +1,26 @@
-import React, { Component }  from 'react';
-const Navabr=()=>
+import React from 'react';
+
+const Navbar=()=>
 {
     return(
+        <div className="navbar">
     <nav>
-       
+        <li>
+            <select>
+              
+                <option>Furniture </option>
+                <option>Furniture </option>
+                <option>Furniture </option>
+                <option>Furniture </option>
+                <option>Furniture </option>
+            </select>
+         </li>
+         </nav>
+    </div>
 
-    </nav>
-    )
+      
+
+    
+    );
 }
+export default Navbar;
