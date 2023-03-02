24332e4ed798c6846f1ef579ffd9d3b7c1628eff
unknown <meghabm193@gmail.com>
2023-03-02 12:53:37 +0530

    pushing

---
+++ src/Megha/navbar/nav.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+function OnlineNav() {
+    return (
+        <div>
+            <div className='nav-parent'>
+            <div className='nav-logo'>
+
+            </div>
+            <div className='nav-flex1'>
+
+            </div>
+            <div className='nav-search'>
+
+            </div>
+            <div className='nav-flex2'>
+
+            </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default OnlineNav
---
+++ src/Megha/online.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function OnlieShopping() {
+    return (
+        <div>
+            
+            
+        </div>
+    )
+}
+
+export default OnlieShopping
